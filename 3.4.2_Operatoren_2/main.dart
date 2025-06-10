888cf303b05a7826fe6ebaac5523744cacae16b7
Kathrine <101878099+dkathrine@users.noreply.github.com>
2025-06-10 14:49:52 +0200

    created main.dart
    - created main.dart
    - setup the main.dart file
    - added variables to store user inputs
    - added if/else-statement to check if the user is allowed to watch a movie

---
+++ 3.4.2_Operatoren_2/main.dart
@@ -0,0 +1,21 @@
+import "dart:io";
+
+void main() {
+  /* 
+    asking for age, parental consent & move age rating
+    & saving the input into variables
+  */
+  print("Type any age:");
+  int age = int.parse(stdin.readLineSync()!);
+  print("Do you have parental consent? (true/false)");
+  bool hasParentalConsent = stdin.readLineSync()!.toLowerCase() == 'true';
+  print("Enter the movie's age rating:");
+  int movieAgeRating = int.parse(stdin.readLineSync()!);
+
+  /* using the stored user inputs, it checks if the person is allowed to watch the movie or not */
+  if (age >= movieAgeRating || hasParentalConsent) {
+    print("you are allowed to watch the movie.");
+  } else {
+    print("you are not allowed to watch the movie.");
+  }
+}
