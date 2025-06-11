efd73e12dd9433cb2adb7c35c3151ce035f7ae57
Kathrine <101878099+dkathrine@users.noreply.github.com>
2025-06-11 14:35:21 +0200

    created and solved tasksheet 3.4.3
    - added the code from the tasksheet into this file - added if-null-operator to check if the weatherData is null and replace the value if true - removed the nullable-variable in the second list - replaced the 3 in the avgTemp calculation with the length of the list temps - also added a bunch of code in multiple comments for a advanced solution with explanations to what I did

---
+++ 3.4.3_Null-Safety/main.dart
@@ -0,0 +1,55 @@
+void main() {
+  List<Map<String, double?>> weatherData = [
+    {'temp': 5.3, 'rain': 0.9, 'wind': null},
+    {'temp': 4.5, 'rain': null, 'wind': 16.8},
+    {'temp': null, 'rain': 3.8, 'wind': null},
+  ];
+
+  /* --------------------------------more advanced solution--------------------------- */
+  /* 
+  before I knew that we shouldn't use loops these were my two solutions 
+  at first we loop through the first List of weatherData and add the temprature values to a new list called temps if the value is null a 0 is added instead
+*/
+  /* List<double?> temps = [];
+
+  weatherData.forEach((data) {
+    temps.add(data['temp'] ?? 0);
+  }); */
+
+  /* 
+  then we calculate the average temperature by iterating through the temps list using the reduce method, adding up all the values, and dividing by the length of the list.
+*/
+  /* double? averageTemp =
+      (temps.reduce((a, b) => (a ?? 0) + (b ?? 0)) ?? 0) / temps.length; */
+
+  /* 
+  I wrote down a second solution using a forEach loop to do the same as the .reduce method, since we didn't talk about .reduce in the course yet (I somehow thought we already did talk about loops tho)
+*/
+  /*   double sum = 0;
+
+  temps.forEach((temp) {
+    if (temp != null) {
+      sum += temp;
+    }
+  });
+
+  double? averageTemp = sum / temps.length; */
+
+  /* 
+  anyway, I then printed the average temperature to the console
+*/
+  /*   print('Durchschnittstemperatur: ${averageTemp.toStringAsFixed(2)}'); */
+  /* -------------------------------------------------------------------------------- */
+
+  /* removed ? to ensure the list of temps has no null-values */
+  List<double> temps = [];
+
+  /* adding 0 to the list if value is null */
+  temps.add(weatherData[0]['temp'] ?? 0);
+  temps.add(weatherData[1]['temp'] ?? 0);
+  temps.add(weatherData[2]['temp'] ?? 0);
+
+  /* calculating the averageTemp by adding up the values and dividing them by the length of the list */
+  double avgTemp = (temps[0] + temps[1] + temps[2]) / temps.length;
+  print('Durchschnittstemperatur: ${avgTemp.toStringAsFixed(2)}');
+}
